82211b3297235e319433abd9a1fe9d330887c245
Abdoulaye SOUMARE <abdoulaye.soumare@devoteam.com>
2013-11-26 01:48:18 +0100

    Reecriture des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ConsultantsDri::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root "static_pages#home"
+  match "/aide", to: "static_pages#help", via: "get"
+  match "/a-propos", to: "static_pages#about", via: "get"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
