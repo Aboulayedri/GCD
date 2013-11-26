290fb29fd9bf946a13e2a8d53c6c62ceb0ab0b0a
Abdoulaye SOUMARE <abdoulaye.soumare@devoteam.com>
2013-11-26 15:41:16 +0100

    Fin validation des entités

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: entities
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Entity < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 end
